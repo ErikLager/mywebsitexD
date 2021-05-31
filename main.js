d422fa306d2887b4b02bca6434de5e526c9b64ee
Erik <43432787+ErikLager@users.noreply.github.com>
2021-05-31 10:17:38 +0200

    added german

--- main.js
+++ main.js
@@ -1,1 +1,2 @@
-alert("helo wrld")
+alert("helo wrld");
+alert("schwerer panzerspähwagen sieben komma fünf zentimeter sonderkraftfahrzeug zweihundertvier und dreissig / vier panzerabwehrkanonen wagen.");
